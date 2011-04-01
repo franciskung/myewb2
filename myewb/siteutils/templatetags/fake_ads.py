28eae2e4061c002c03a31f0d5decdddf38539061
Francis Kung <franciskung@ewb.ca>
2011-04-01 03:36:50 -0400

    More fake ads

--- myewb/siteutils/templatetags/fake_ads.py
+++ myewb/siteutils/templatetags/fake_ads.py
@@ -13,7 +13,7 @@ def fake_ad():
     ads = os.listdir(templatedir)
     adname = choice(ads)
 
-    context = {}
+    context = {'STATIC_URL': settings.STATIC_URL}
     ad = loader.get_template("fake_ads/%s" % adname).render(Context(context))
     
     return ad
