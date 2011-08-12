338552fac35608596c4a5ddf1f95e773b640d2bf
Francis Kung <franciskung@ewb.ca>
2011-08-12 14:44:05 -0400

    "ewbook" april fool's theme

---
+++ myewb/siteutils/templatetags/fake_ads.py
@@ -0,0 +1,20 @@
+from django.template import Context, loader
+from django import template
+
+import settings
+import os
+from random import choice
+
+register = template.Library()
+
+@register.simple_tag
+def fake_ad():
+    templatedir = os.path.join(settings.PROJECT_ROOT, "templates", "ewbbook", "fake_ads")
+    ads = os.listdir(templatedir)
+    adname = choice(ads)
+
+    context = {'STATIC_URL': settings.STATIC_URL}
+    ad = loader.get_template("fake_ads/%s" % adname).render(Context(context))
+    
+    return ad
+
