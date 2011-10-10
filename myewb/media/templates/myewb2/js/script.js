07a5b8755ac0c2f20eb3d40eda81cbb6fd653b05
francis <francis@accra.ewb.ca>
2011-10-10 11:51:06 -0400

    More new-frontpage work

--- myewb/media/templates/myewb2/js/script.js
+++ myewb/media/templates/myewb2/js/script.js
@@ -1,10 +1,31 @@
 /* Author:
-
+Francis & Sean & friends
+October 2011
 */
 
 
 
+// delay-load of avatar images
+// with many props to http://24ways.org/2010/speed-up-your-site-with-delayed-content
+
+function runDelayedAvatarLoad() {
+
+  $('[data-background-src]').css("background-image", function(index){
+    console.log($(this).attr('data-background-src'));
+    return "url(" + $(this).attr('data-background-src') + ")";
+
+  });  
+
+
+}
 
 
 
 
+
+$(document).ready(function() {
+
+  runDelayedAvatarLoad();
+  // this is also run again by the frontpage's Ajax post loader
+
+});
