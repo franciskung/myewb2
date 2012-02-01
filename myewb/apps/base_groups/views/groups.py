903c8c4c2a7959c306c514b9d58c5fc4593c3151
Francis Kung <franciskung@ewb.ca>
2012-02-01 17:27:02 -0500

    More concurrency fixes...

--- myewb/apps/base_groups/views/groups.py
+++ myewb/apps/base_groups/views/groups.py
@@ -194,7 +194,14 @@ def group_detail(request, group_slug, model=None, member_model=None,
                 m.delete()
             member = members[0]
     elif group.user_is_pending_member(request.user):
-        member = group.pending_members.get(user=request.user)
+        try:
+            member = group.pending_members.get(user=request.user)
+        except MultipleObjectsReturned:
+            members = group.pending_members.filter(user=request.user)
+            for m in members[1:]:
+                m.delete()
+            member = members[0]
+            
     else:
         member = None
         
